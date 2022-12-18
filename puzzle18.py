5920ea722ed5ecb91f5497da97a3ca5206f634b6
Diego Diaz <valthalion@gmail.com>
2022-12-18 17:24:01 +0100

    Day 18, part 2

--- puzzle18.py
+++ puzzle18.py
@@ -23,15 +23,64 @@ def build_graph(cubes):
     return graph
 
 
+def  build_complement_graph(exclusion_graph):
+    graph = {}
+    xlow, xhigh = min(x for x, _, _ in exclusion_graph), max(x for x, _, _ in exclusion_graph)
+    ylow, yhigh = min(y for _, y, _ in exclusion_graph), max(y for _, y, _ in exclusion_graph)
+    zlow, zhigh = min(z for _, _, z in exclusion_graph), max(z for _, _, z in exclusion_graph)
+    for x in range(xlow - 1, xhigh + 2):
+        for y in range(ylow - 1, yhigh + 2):
+            for z in range(zlow - 1, zhigh + 2):
+                cube = (x, y, z)
+                if cube in exclusion_graph:
+                    continue
+                graph[cube] = set(
+                    neighbour for neighbour in neighbours(cube) if neighbour not in exclusion_graph
+                )
+    return graph
+
+
+def connected_components(graph, exculsion_graph):
+    xlow = min(x for x, _, _ in graph)
+    ylow = min(y for _, y, _ in graph)
+    zlow = min(z for _, _, z in graph)
+    nodes = set(graph)
+    outer_point = (xlow, ylow, zlow)
+    nodes.remove(outer_point)
+    queue = {outer_point}
+    while queue:  # remove outer component (reachable by air)
+        node = queue.pop()
+        node_neighbours = graph[node] & nodes
+        queue |= node_neighbours
+        nodes -= node_neighbours
+    while nodes:
+        component = set()
+        queue = {nodes.pop()}
+        while queue:
+            node = queue.pop()
+            component.add(node)
+            node_neighbours = graph[node] & nodes
+            queue |= node_neighbours
+            nodes -= node_neighbours
+        yield {cube: neighbours for cube, neighbours in graph.items() if cube in component}
+
+
+def free_surface_area(graph):
+    return sum(6 - len(neighbours) for neighbours in graph.values())
+
+
 def part_1():
     graph = build_graph(read_data())
-    surface_area = sum(6 - len(neighbours) for neighbours in graph.values())
+    surface_area = free_surface_area(graph)
     return surface_area
 
 
 def part_2():
     lava_graph = build_graph(read_data())
     air_graph = build_complement_graph(lava_graph)
-    # connected components of air_graph
-    # remove outer component
-    # take free surfaces of air components from free surfaces of lava graph
+    air_pockets = connected_components(air_graph, lava_graph)
+    total_surface_area = free_surface_area(lava_graph)
+    air_pockets_surface_area = sum(
+        free_surface_area(component) for component in connected_components(air_graph, lava_graph)
+    )
+    return total_surface_area - air_pockets_surface_area
